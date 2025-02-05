7fad834627bba40f6214d12bf1c96adae3be9da9
Guilherme da Silva <guibs.email@gmail.com>
2025-02-05 13:40:20 -0500

    feat: add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import '@/index.css'
 
+import { Analytics } from '@vercel/analytics/react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ThemeProvider } from '@/components/theme-provider'
@@ -9,6 +10,7 @@ import { App } from '@/App'
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
+		<Analytics />
 		<main className="mx-auto mt-11 p-5 max-w-3xl font-geist h-screen text-neutral-900 dark:text-neutral-200">
 			<ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
 				<Navbar />
